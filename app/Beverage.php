446cc5e3689faedc70489dfffe38fd3024871f0d
Мирослав Здравковић <miroslav@fimanager.com>
2018-07-25 13:57:31 +0200

    Minor user can't buy an alcohol drink

--- app/Beverage.php
+++ app/Beverage.php
@@ -3,8 +3,16 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Exceptions\MinorCannotBuyAlcoholicBeverageException;
 
 class Beverage extends Model
 {
-    //
+    public function buy()
+    {
+        if (auth()->user()->isMinor()) {
+            throw new MinorCannotBuyAlcoholicBeverageException;
+        }
+
+        return true;
+    }
 }
