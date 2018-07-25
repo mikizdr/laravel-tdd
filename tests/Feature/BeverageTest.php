954fa84ddab54d76428df3c35b1b9e6629f56632
Мирослав Здравковић <miroslav@fimanager.com>
2018-07-25 16:21:55 +0200

    Fixes #2: A user can see a single beverage

--- tests/Feature/BeverageTest.php
+++ tests/Feature/BeverageTest.php
@@ -14,18 +14,36 @@ class BeverageTest extends TestCase
     use RefreshDatabase;
 
     /**
+    * @var Beverage
+    */
+    private $beverage;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->beverage = factory(Beverage::class)->create();
+    }
+
+    /**
      * An user can visit a beverage page and views beverages.
      *
      * @test
      */
     public function An_user_can_visit_a_beverage_page_and_views_beverages()
     {
-        $beverage = factory(Beverage::class)->create();
         // user will go to an url
         $response = $this->get('beverage');
         // get beverage name
-        $response->assertSee($beverage->name);
+        $response->assertSee($this->beverage->name);
         // assert status
+        $response->assertStatus(200);
+    }
+
+    /** @test */
+    public function a_user_can_visit_a_single_beverage_page()
+    {
+        $response = $this->get('/beverage/' . $this->beverage->id);
+        $response->assertSee($this->beverage->name);
         $response->assertStatus(200);
     }
 
